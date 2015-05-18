ce7cac4105f74916291d5d179cd8779273365b04
David Shea <dshea@redhat.com>
2015-05-18 14:03:48 -0400

    Add python version classifiers to the package info

--- setup.py
+++ setup.py
@@ -24,5 +24,7 @@ setup(
                  'Environment :: Plugins',
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4'
                  )
     )
