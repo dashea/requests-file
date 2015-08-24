90c84fa261918d3b73d96b882370f1b5266fbb57
David Shea <dshea@redhat.com>
2015-08-24 14:41:13 -0400

    Handle files with a drive component.
    Add handling for URLs with a drive component, including the old-style
    Windows URLs that use a pipe as the drive separator
    (e.g., file://C|/path/to/file).
    
    Also fix the URLs used in the test cases to properly encode drives and
    directory separators that are not /.
    
    Resolves #2.

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,11 +12,30 @@ class FileRequestTestCase(unittest.TestCase):
         self._session = requests.Session()
         self._session.mount("file://", FileAdapter())
 
+    def _pathToURL(self, path):
+        """Convert a filesystem path to a URL path"""
+        urldrive, urlpath = os.path.splitdrive(path)
+
+        # Split the path on the os spearator and recombine it with / as the
+        # separator. There probably aren't any OS's that allow / as a path
+        # component, but just in case, encode any remaining /'s.
+        urlsplit = (part.replace('/', '%2F') for part in urlpath.split(os.sep))
+        urlpath = "/".join(urlsplit)
+
+        # Encode /'s in the drive for the imaginary case where that can be a thing
+        urldrive = urldrive.replace('/', '%2F')
+
+        # Add the leading /. If there is a drive component, this needs to be
+        # placed before the drive.
+        urldrive = "/" + urldrive
+
+        return urldrive + urlpath
+
     def test_fetch_regular(self):
         # Fetch this file using requests
         with open(__file__, "rb") as f:
             testdata = f.read()
-        response = self._session.get("file://%s" % os.path.abspath(__file__))
+        response = self._session.get("file://%s" % self._pathToURL(os.path.abspath(__file__)))
 
         self.assertEqual(response.status_code, requests.codes.ok)
         self.assertEqual(response.headers['Content-Length'], len(testdata))
@@ -36,7 +55,7 @@ class FileRequestTestCase(unittest.TestCase):
         # probably doesn't work on windows
         with tempfile.NamedTemporaryFile() as tmp:
             os.chmod(tmp.name, 0)
-            response = self._session.get("file://%s" % os.path.abspath(tmp.name))
+            response = self._session.get("file://%s" % self._pathToURL(os.path.abspath(tmp.name)))
 
             self.assertEqual(response.status_code, requests.codes.forbidden)
             self.assertTrue(response.text)
@@ -62,7 +81,7 @@ class FileRequestTestCase(unittest.TestCase):
     def test_head(self):
         # Check that HEAD returns the content-length
         testlen = os.stat(__file__).st_size
-        response = self._session.head("file://%s" % os.path.abspath(__file__))
+        response = self._session.head("file://%s" % self._pathToURL(os.path.abspath(__file__)))
 
         self.assertEqual(response.status_code, requests.codes.ok)
         self.assertEqual(response.headers['Content-Length'], testlen)
@@ -72,19 +91,20 @@ class FileRequestTestCase(unittest.TestCase):
     def test_fetch_post(self):
         # Make sure that non-GET methods are rejected
         self.assertRaises(ValueError, self._session.post,
-                ("file://%s" % os.path.abspath(__file__)))
+                ("file://%s" % self._pathToURL(os.path.abspath(__file__))))
 
     def test_fetch_nonlocal(self):
         # Make sure that network locations are rejected
         self.assertRaises(ValueError, self._session.get,
-                ("file://example.com%s" % os.path.abspath(__file__)))
+                ("file://example.com%s" % self._pathToURL(os.path.abspath(__file__))))
         self.assertRaises(ValueError, self._session.get,
-                ("file://localhost:8080%s" % os.path.abspath(__file__)))
+                ("file://localhost:8080%s" % self._pathToURL(os.path.abspath(__file__))))
 
         # localhost is ok, though
         with open(__file__, "rb") as f:
             testdata = f.read()
-        response = self._session.get("file://localhost%s" % os.path.abspath(__file__))
+        response = self._session.get("file://localhost%s" % self._pathToURL(os.path.abspath(__file__)))
+        self.assertEqual(response.status_code, requests.codes.ok)
         self.assertEqual(response.content, testdata)
         response.close()
 
@@ -96,7 +116,7 @@ class FileRequestTestCase(unittest.TestCase):
             with open(os.path.join(tmpdir, 'spa ces'), "w+b") as space_file:
                 space_file.write(testdata)
                 space_file.flush()
-                response = self._session.get("file://%s/spa%%20ces" % tmpdir)
+                response = self._session.get("file://%s/spa%%20ces" % self._pathToURL(tmpdir))
                 self.assertEqual(response.status_code, requests.codes.ok)
                 self.assertEqual(response.content, testdata)
                 response.close()
@@ -104,14 +124,14 @@ class FileRequestTestCase(unittest.TestCase):
             with open(os.path.join(tmpdir, 'per%cent'), "w+b") as percent_file:
                 percent_file.write(testdata)
                 percent_file.flush()
-                response = self._session.get("file://%s/per%%25cent" % tmpdir)
+                response = self._session.get("file://%s/per%%25cent" % self._pathToURL(tmpdir))
                 self.assertEqual(response.status_code, requests.codes.ok)
                 self.assertEqual(response.content, testdata)
                 response.close()
 
             # percent-encoded directory separators should be rejected
             with open(os.path.join(tmpdir, 'badname'), "w+b") as bad_file:
-                response = self._session.get("file://%s%%2Fbadname" % tmpdir)
+                response = self._session.get("file://%s%%%Xbadname" % (self._pathToURL(tmpdir), ord(os.sep)))
                 self.assertEqual(response.status_code, requests.codes.not_found)
                 response.close()
 
@@ -120,7 +140,7 @@ class FileRequestTestCase(unittest.TestCase):
 
     def test_close(self):
         # Open a request for this file
-        response = self._session.get("file://%s" % os.path.abspath(__file__))
+        response = self._session.get("file://%s" % self._pathToURL(os.path.abspath(__file__)))
 
         # Try closing it
         response.close()
@@ -128,4 +148,17 @@ class FileRequestTestCase(unittest.TestCase):
     def test_missing_close(self):
         # Make sure non-200 responses can be closed
         response = self._session.get("file:///no/such/path")
+        response.close()
+
+    @unittest.skipIf(platform.system() != "Windows", "skipping windows URL test")
+    def test_windows_legacy(self):
+        """Test |-encoded drive characters on Windows"""
+        with open(__file__, "rb") as f:
+            testdata = f.read()
+
+        drive, path = os.path.splitdrive(os.path.abspath(__file__))
+        response = self._session.get("file:///%s|%s" % (drive[:-1], path.replace(os.sep, "/")))
+        self.assertEqual(response.status_code, requests.codes.ok)
+        self.assertEqual(response.headers['Content-Length'], len(testdata))
+        self.assertEqual(response.content, testdata)
         response.close()
