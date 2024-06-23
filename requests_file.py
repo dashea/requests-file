8c29b01e1d47da953a5e7eb6b962943fc7dfef06
huakim <fijik19@gmail.com>
2024-06-23 07:30:49 +0000

    update

--- requests_file.py
+++ requests_file.py
@@ -2,12 +2,10 @@ from requests.adapters import BaseAdapter
 from requests.compat import urlparse, unquote
 from requests import Response, codes
 import errno
-import os
 import stat
 import locale
-import io
 import glob
-import urllib
+from urllib.parse import parse_qs as parse_query
 import math
 
 try:
@@ -15,7 +13,173 @@ try:
 except ImportError:
     from StringIO import StringIO as BytesIO
 
-parse_query = urllib.parse.parse_qs
+from sortedcontainers import SortedSet
+import io
+import os
+
+class F:
+  def __init__(self, file):
+    self.file = file
+  def __getattr__(self, name):
+    return getattr(self.file, name)
+  def __eq__(self, a):
+    return (self.begin <= a) and (self.end > a)
+  def __gt__(self, a):
+    return (self.begin > a)
+  def __lt__(self, a):
+    return not (self >= a)
+  def __ge__(self, a):
+    return (self.begin > a) or (self.end > a)
+  def __le__(self, a):
+    return not (self > a)
+  def __ne__(self, a):
+    return not (self == a)
+  def __hash__(self):
+    begin = self.begin
+    end = self.end
+    return ((begin + end) << 4) + (end - begin)
+
+def FilesIO(file_names):
+    files = SortedSet()
+    length = 0
+    prev = None
+
+    for index, file in enumerate(file_names):
+        file = io.open(file, 'rb')
+        file.index = index
+        if prev != None:
+            prev.next = file
+        file.prev = prev
+        file.next = None
+        prev = file
+        st_size = file.len = os.fstat(file.fileno()).st_size
+        file.begin = length
+        length += st_size
+        file.end = length
+        files.add(F(file))
+
+    current_offset = 0
+    current_file = files[0]
+    isclosed = False
+
+    methods = {}
+
+    def add(func):
+        name = func.__name__
+        func = staticmethod(func)
+        methods[name] = func
+        return func
+
+    @add
+    def close():
+        nonlocal isclosed, files
+        if not isclosed:
+            for i in files:
+                i.close()
+            isclosed = True
+    @add
+    def closed():
+        nonlocal isclosed
+        return isclosed
+    @add
+    def readable():
+        if closed():
+            raise ValueError('I/O operation on closed file')
+        return True
+    methods['seekable'] = readable
+    def tostring(self):
+        return '<FilesIO('+repr(file_names)+') at '+hex(id(self))+'>'
+    @add
+    def fileno():
+        return OSError('not supported')
+    @add
+    def flush():
+        pass
+    @add
+    def isatty():
+        return False
+    @add
+    def tell():
+        nonlocal current_offset
+        return current_offset
+    @add
+    def writable():
+        return not readable()
+    @add
+    def seek(offset, whence=0):
+        nonlocal current_offset, length
+        if whence == 1:
+            offset += current_offset
+        elif whence == 2:
+            offset += length
+        if offset < 0:
+            offset = 0
+        return set_offset(offset)
+
+    def set_offset(offset):
+        nonlocal current_offset, current_file, files
+        if offset > length:
+            current_offset = length
+            current_file = files[-1]
+        elif offset == 0:
+            current_file = files[0]
+            current_file.seek(0)
+            current_offset = 0
+            return 0
+        else:
+            current_offset = offset
+            current_file = search_file(offset)
+        current_file.seek(current_offset - current_file.begin)
+        return current_offset
+
+    def search_file(offset):
+        nonlocal files
+        try:
+            return files[files.index(offset)]
+        except ValueError:
+            return files[-1]
+    @add
+    def readinto(ret):
+        nonlocal current_file, current_offset, length
+        readable()
+        size = len(ret)
+        default_size = size
+        view = memoryview(ret)
+        index = 0
+        while size > 0:
+            available = current_file.end - current_offset
+            if size > available:
+                size -= available
+                current_file.readinto(view[index : (index + available)])
+                index += available
+                next_file = current_file.next
+                current_offset = current_file.end
+                if next_file is None:
+                    return default_size - size
+                else:
+                    next_file.seek(0)
+                    current_file = next_file
+            else:
+                current_file.readinto(view[index : (index + size)])
+                current_offset += size
+                return default_size
+
+    @add
+    def read(size = -1):
+        nonlocal current_offset, length
+        if size < 0:
+            size = length - current_offset
+        ret = bytearray(size)
+        size = readinto(ret)
+        return bytes(memoryview(ret)[:size])
+    @add
+    def readall():
+        nonlocal length
+        return read(length)
+    methods['len']=length
+    methods['__str__'] = tostring
+    methods['__repr__'] = tostring
+    return type('FilesIO', (io.RawIOBase,),methods)()
 
 class FileAdapter(BaseAdapter):
     def __init__(self, set_content_length=True, query={}):
@@ -69,11 +233,13 @@ class FileAdapter(BaseAdapter):
                 raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
         else:
             files = [path]
-        line = b''.join([io.open(path, 'rb').read() for path in files])
-        text_encoding = self.get_flag_val(query, 'text_encoding')
-        if text_encoding:
-            line = line.decode(text_encoding.lower()).encode('utf-8')
-        return BytesIO(line)
+        if len(files) == 1:
+            raw = io.open(files[0], 'rb')
+            resp_stat = os.fstat(raw.fileno())
+            raw.len = resp_stat.st_size
+            return raw
+        else:
+            return FilesIO(files)
 
 
     def send(self, request, **kwargs):
@@ -175,9 +341,8 @@ class FileAdapter(BaseAdapter):
 
             # If it's a regular file, set the Content-Length
          #   resp_stat = os.fstat(raw.fileno())
-         #   if stat.S_ISREG(resp_stat.st_mode) and self._set_content_length:
-       #     resp.headers["Content-Length"] = resp_stat.st_size
-            resp.headers["Content-Length"] = len(raw.getbuffer())
+            if self._set_content_length:
+                resp.headers["Content-Length"] = raw.len
 
         return resp
 
